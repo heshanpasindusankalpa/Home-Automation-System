73fe3dfe0ccc7e16d315c121bf4cb793c47526c2
heshanpasindusankalpa <pasindusankalpa2021@gmail.com>
2024-12-23 20:01:30 +0530

    Pages folder

---
+++ web/src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+//import NavBar from '../../components/Navigation/NavBar'
+
+export default function Home() {
+  return (
+    <div>
+        <NavBar/>
+      
+    </div>
+  )
+}
